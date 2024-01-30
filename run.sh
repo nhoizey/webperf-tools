988e5241ca726eb0f8ce42ea43443685920f6e70
Nicolas Hoizey <nicolas@hoizey.com>
2024-01-30 14:49:23 +0100

    Add graphs

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+./web-vitals-graphs.js graphs --domain www.clever-age.com --device phone --reverse
+./web-vitals-graphs.js graphs --domain www.clever-age.com --device desktop --reverse
+
+./web-vitals-graphs.js graphs --domain www.belambra.fr --device phone --country fr --reverse
+./web-vitals-graphs.js graphs --domain www.belambra.fr --device desktop --country fr --reverse
+
+./web-vitals-graphs.js graphs --domain www.leaderplant.com --device phone --country fr --reverse
+./web-vitals-graphs.js graphs --domain www.leaderplant.com --device desktop --country fr --reverse
+
+./web-vitals-graphs.js graphs --domain www.auto-ies.com --device phone --country fr --reverse
+./web-vitals-graphs.js graphs --domain www.auto-ies.com --device desktop --country fr --reverse
+
+./web-vitals-graphs.js graphs --domain fr.bic.com --device phone --country fr --reverse
+./web-vitals-graphs.js graphs --domain fr.bic.com --device desktop --country fr --reverse
+./web-vitals-graphs.js graphs --domain us.bic.com --device phone --country us --reverse
+./web-vitals-graphs.js graphs --domain us.bic.com --device desktop --country us --reverse
+
+./web-vitals-graphs.js graphs --domain www.easycash.fr --device phone --country fr --reverse
+./web-vitals-graphs.js graphs --domain www.easycash.fr --device desktop --country fr --reverse
+./web-vitals-graphs.js graphs --domain bons-plans.easycash.fr --device phone --country fr --reverse
+./web-vitals-graphs.js graphs --domain bons-plans.easycash.fr --device desktop --country fr --reverse
+./web-vitals-graphs.js graphs --domain prix.easycash.fr --device phone --country fr --reverse
+./web-vitals-graphs.js graphs --domain prix.easycash.fr --device desktop --country fr --reverse
+
+./web-vitals-graphs.js graphs --domain www.ynov.com --device phone --country fr --reverse
+./web-vitals-graphs.js graphs --domain www.ynov.com --device desktop --country fr --reverse
